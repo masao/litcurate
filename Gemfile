9eeffc20d8b223650ae18b3eb087be14d60735a2
Masao Takaku <tmasao@acm.org>
2016-08-04 16:53:24 +0000

    OAuth2 login enabled.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ gem "json"
 gem "sinatra"
 gem "sinatra-contrib"
 gem "omniauth"
+gem "omniauth-mendeley_oauth2"
 gem "puma"
 gem "foreman"
 
--- app.rb
+++ app.rb
@@ -1,18 +1,34 @@
 require "erb"
 require "sinatra"
-require "sinatra/config_file"
+require "omniauth"
+require "omniauth-mendeley_oauth2"
 
 require_relative "mendeley.rb"
 
-class App < Sinatra::Base
-  use Rack::Session::Cookie
-  use OmniAuth::Strategies::Developer
-  enable :sessions
+# Workaround for omniauth-oauth2 changes...
+# cf. https://github.com/intridea/omniauth-oauth2/pull/82
+class OmniAuth::Strategies::Mendeley
+  def callback_url
+    options[:redirect_uri] || (full_host + script_name + callback_path)
+  end
+end
 
-  register Sinatra::ConfigFile
-  config_file "mendelurvey.yml"
+class App < Sinatra::Base
+  set :sessions, secret: "LitCurate"
+  use OmniAuth::Builder do
+    provider :mendeley, ENV['MENDELEY_CLIENT_ID'], ENV['MENDELEY_CLIENT_SECRET']
+  end
+
   get "/" do
     erb :index
   end
+
+  get "/auth/:provider/callback" do
+    result = request.env['omniauth.auth']
+    erb "<a href='/'>Top</a><br>
+      <h1>#{params[:provider]}</h1>
+      <pre>#{JSON.pretty_generate(result)}</pre>"
+  end
+
   helpers ERB::Util
 end
