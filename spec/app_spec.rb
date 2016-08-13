ba3003139a3ed8a76b8bb1c0a0555d3571d03e13
Masao Takaku <tmasao@acm.org>
2016-08-13 15:02:47 +0000

    add more spec for error load_annotations

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,18 +12,31 @@ describe App do
     expect(last_response).to be_ok
   end
 
-  it "should load annotations" do
-    get "/load_annotations"
-    expect(last_response).not_to be_ok
-    annotation = FactoryGirl.create(:annotation)
-    env "rack.session", { uid: annotation.uid }
-    get "/load_annotations", {folder: annotation.folder}
-    expect(last_response).to be_ok
-    json = JSON.load(last_response.body)
-    expect(json).to respond_to(:each)
-    obj = json.last
-    expect(obj["uid"]).to eq annotation.uid
-    expect(obj["name"]).to eq annotation.name
-    expect(obj["folder"]).to eq annotation.folder
+  context "/load_annotations" do
+    it "should load annotations" do
+      annotation = FactoryGirl.create(:annotation)
+      env "rack.session", { uid: annotation.uid }
+      get "/load_annotations", {folder: annotation.folder}
+      expect(last_response).to be_ok
+      json = JSON.load(last_response.body)
+      expect(json).to respond_to(:each)
+      obj = json.last
+      expect(obj["uid"]).to eq annotation.uid
+      expect(obj["name"]).to eq annotation.name
+      expect(obj["folder"]).to eq annotation.folder
+    end
+    it "should raise errors with missing parameters" do
+      get "/load_annotations"
+      expect(last_response).not_to be_ok
+      expect(last_response.status).to eq 403
+      obj = JSON.load(last_response.body)
+      expect(obj).to have_key("error")
+      env "rack.session", { uid: "dummy" }
+      get "/load_annotations"
+      expect(last_response).not_to be_ok
+      expect(last_response.status).to eq 400
+      obj = JSON.load(last_response.body)
+      expect(obj).to have_key("error")
+    end
   end
 end
